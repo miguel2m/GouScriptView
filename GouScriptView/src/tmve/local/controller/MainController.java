5784dc3fcbee5b7532c9165b7eb1ae974cf66cc5
Miguelangel <miguelangelmedina2494@gmail.com>
2020-01-22 16:37:32 -0400

    subiendo 7zip

--- GouScriptView/src/tmve/local/controller/MainController.java
+++ GouScriptView/src/tmve/local/controller/MainController.java
@@ -7,7 +7,14 @@ package tmve.local.controller;
 
 import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,53 +26,138 @@ import javafx.scene.image.ImageView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.apache.commons.io.FilenameUtils;
 import tmve.local.main.GouScript;
+import tmve.local.services.UnGzip;
 import tmve.local.view.ViewFactory;
 
 /**
- *
+ * Crontolador principal del la vista principal
  * @author P05144
  */
 public class MainController extends BaseController implements Initializable {
-
-
+    private  File inputDirectory;
+    private String outputDirectory ="."+File.separator+"database";
+    /**
+     * Constructor de la clase principal
+     * @param gouScript
+     * @param viewFactory
+     * @param fxmlName 
+     */
     public MainController(GouScript gouScript, ViewFactory viewFactory, String fxmlName) {
         super(gouScript, viewFactory, fxmlName);
     } 
+    /**
+     * Atributo para importar la base de datos (Import GXPORT)
+     */
     @FXML
     private Label labelGxportInput;
-    
+    /**
+     * Atributo para importar la base de datos (Import GXPORT)
+     */
     @FXML
     private MenuItem helpItem;
-
+    
+    /**
+     * Atributo para importar la base de datos (Import GXPORT)
+     */
+    @FXML
+    private Button gxportButton;
+    /**
+     * Atributo para importar la base de datos (Import GXPORT)
+     */
+    @FXML
+    private TextField textOpenFile;
+    /**
+     * Atributo para importar la base de datos (Import GXPORT)
+     */
+    @FXML
+    private Button fileOpen;
+    
+    
+    /**
+     * METODO que dirije a la ventana de ayuda
+     * @param event 
+     */
     @FXML
     void onHelpItem(ActionEvent event) {
         viewFactory.showHelpWindown();
     }
     
-    @FXML
-    private Button gxportButton;
-
-    @FXML
-    void gxportDataBase(ActionEvent event) {
-
-    }
-    
-    @FXML
-    private TextField textOpenFile;
-    @FXML
-    private Button fileOpen;
-    
+    /**
+     * METODO que se activa cuando el usuario presiona el boton de abrir carpeta
+     * para seleccionar el directorio destino de la BASE DE DATOS
+     * @param event 
+     */
     @FXML
     void onFileOpen(ActionEvent event) {
         DirectoryChooser direcotryChooser = new DirectoryChooser();
         direcotryChooser.setTitle("Import GXPORT Data Base");
         Stage stage = (Stage)labelGxportInput.getScene().getWindow();
-        File file = direcotryChooser.showDialog(stage);
-        if(file!=null)
-            textOpenFile.setText(file.getAbsolutePath());
+        inputDirectory = direcotryChooser.showDialog(stage);
+        if(inputDirectory!=null)
+            textOpenFile.setText(inputDirectory.getAbsolutePath());
     }
     
+    /**
+     * METODO que se activa cuando el usuario presiona crear DATA BASE
+     * @param event 
+     */
+     @FXML
+    void onGxportDataBase(ActionEvent event) {
+        if(!textOpenFile.getText().equals(null)){
+            gxportButton.setDisable(true);
+            //Is path is readable
+            Path file = Paths.get(textOpenFile.getText());
+            if(Files.isReadable(file)){
+                File directory = new File(outputDirectory);
+                if (!directory.exists()){
+                    directory.mkdir();
+                    File extractDirectory = new File(outputDirectory+File.separator+"extract");
+                    if (!extractDirectory.exists())
+                        extractDirectory.mkdir();                   
+                }               
+                
+                //get all the files from a directory
+                File[] fList = inputDirectory.listFiles();
+                
+                for (File f : fList) {
+                    String ext = FilenameUtils.getExtension(f.getName());
+                    
+                    if (ext.equals("gz")) {
+                        //UnGzip.gunzipIt(f.getAbsolutePath(),outputDirectory+"/extract");gxportButton
+
+                        UnGzip ungz = new UnGzip(f.getAbsolutePath(),outputDirectory+File.separator+"extract");
+
+                        ungz.start();
+
+                        ungz.setOnSucceeded(e -> {
+
+                            System.out.println(f.getAbsolutePath() + " DONE");
+
+                            // TODO, . . . 
+                            // You can modify any GUI element from here...
+                            // ...with the values you got from the service
+                        });
+                                           
+                    }
+                }
+            }
+        }
+         
+    }
+    
+    /**
+     * Metodo paa cerrar el programa
+     * @param event 
+     */
+    @FXML
+    void onExit(ActionEvent event) {
+        Stage stage = (Stage)textOpenFile.getScene().getWindow();
+        viewFactory.closeStage(stage);  
+       
+        
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
---
+++ GouScriptView/src/tmve/local/services/UnGzip.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tmve.local.services;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import org.apache.commons.io.FilenameUtils;
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.stream.LogOutputStream;
+
+/**
+ *
+ * @author P05144
+ */
+public class UnGzip extends Service{
+
+    private String INPUT_GZIP_FILE;
+    private String OUTPUT_FILE;
+
+    public UnGzip(String INPUT_GZIP_FILE, String OUTPUT_FILE) {
+        this.INPUT_GZIP_FILE = INPUT_GZIP_FILE;
+        this.OUTPUT_FILE = OUTPUT_FILE;
+    }
+    
+    
+    
+    
+    /**
+     * GunZip it
+     */
+    public void gunzipIt(){
+ 
+     byte[] buffer = new byte[2048];
+ 
+     try{
+ 
+    	 GZIPInputStream gzis = 
+    		new GZIPInputStream(new FileInputStream(INPUT_GZIP_FILE));
+ 
+    	 FileOutputStream out = 
+            new FileOutputStream(OUTPUT_FILE+"/"+FilenameUtils.getBaseName(INPUT_GZIP_FILE));
+ 
+        int len;
+        while ((len = gzis.read(buffer)) > 0) {
+        	out.write(buffer, 0, len);
+        }
+ 
+        gzis.close();
+    	out.close();
+ 
+    	System.out.println("Done");
+    	
+    }catch(IOException ex){
+       ex.printStackTrace();   
+    }
+   } 
+    
+    public void unGz() {
+        try{
+                int value = new ProcessExecutor().command(
+                        "."+File.separator+"lib"+File.separator+"7z.exe",
+                        "x",
+                        INPUT_GZIP_FILE,
+                        "-so",
+                        ">",
+                        OUTPUT_FILE+File.separator+FilenameUtils.getBaseName(INPUT_GZIP_FILE)
+                )
+                        .readOutput(true)
+                        .redirectOutput(new LogOutputStream() {
+                                @Override
+                                protected void processLine(String line) {
+                                    //text=line;
+                                    // Update UI here.
+                                   
+                                    System.out.println("line "+line);
+                                    //label=line;
+                                     //System.out.println(" "+line);
+                                    //System.out.println(" "+line);
+                                }
+                            })
+                        .destroyOnExit()
+                        .execute()
+                        .getExitValue();
+                if (value == 0) {
+                    System.out.println("Done");
+                }
+        }catch(Exception e){
+            System.out.println(" "+e.getMessage());
+        }
+    }
+    
+    @Override
+    protected Task createTask() {
+         return new Task() {
+            @Override
+            protected Void call() throws Exception {
+                unGz();
+                //gunzipIt();
+                return null;
+            }
+        };
+    }
+    
+}
