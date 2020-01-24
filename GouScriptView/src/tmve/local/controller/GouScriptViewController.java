e9cd9f86acdbaad9949d097548c67f5cd63a89aa
Miguelangel <miguelangelmedina2494@gmail.com>
2020-01-24 16:44:43 -0400

    GousCript form

--- GouScriptView/src/tmve/local/controller/GouScriptViewController.java
+++ GouScriptView/src/tmve/local/controller/GouScriptViewController.java
@@ -8,14 +8,20 @@ package tmve.local.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import org.apache.commons.collections4.CollectionUtils;
+import org.controlsfx.control.CheckComboBox;
+import org.controlsfx.control.textfield.AutoCompletionBinding;
 import org.controlsfx.control.textfield.TextFields;
 import tmve.local.services.AdjnodeCsv;
 
@@ -26,21 +32,27 @@ import tmve.local.services.AdjnodeCsv;
  */
 public class GouScriptViewController implements Initializable {
     
-    private StringBuilder rncSearchName = new StringBuilder("");
+    private StringBuffer rncSearchName = new StringBuffer("");
     private ObservableList<String> rncPossibleSuggestions = FXCollections.observableArrayList();
-    
+    private ObservableList<String> nodebRnc = FXCollections.observableArrayList();
+    private AutoCompletionBinding auto;
+    @FXML
+    private Button cargarNodeB;
+    @FXML
+    private CheckComboBox<String> nodebList;
     @FXML
     private TextField searchRnc;
     
     @FXML
     void onTextRncFieldKeyTyped(KeyEvent   event) {
+        cargarNodeB.setDisable(true);
             if(searchRnc.getText().trim().isEmpty()|| searchRnc.getText() == null)
-                rncSearchName =  new StringBuilder("");
-            
-            rncSearchName.append(event.getCharacter());
-           System.out.println(""+rncSearchName);
-           if(rncSearchName.length()==3){
-            
+                rncSearchName =  new StringBuffer("");
+            //rncSearchName.
+           rncSearchName.append(event.getCharacter());
+           System.out.println(rncSearchName+" "+searchRnc.getText().trim().length());
+           if(searchRnc.getText().length()>1 &&searchRnc.getText().length()<=4 ){
+           
             AdjnodeCsv adjnodeCsv = new AdjnodeCsv(rncSearchName.toString(),false);
             
             adjnodeCsv.start();
@@ -54,26 +66,60 @@ public class GouScriptViewController implements Initializable {
                         rncPossibleSuggestions.add(t);
                     });
                 }
-               // System.out.println("VALUE "+adjnodeCsv.getValue());  
-               TextFields.bindAutoCompletion(searchRnc,rncPossibleSuggestions);
+                System.out.println("VALUE "+adjnodeCsv.getValue());  
+                auto = TextFields.bindAutoCompletion(searchRnc, rncPossibleSuggestions);
+                
+                /*AdjnodeCsv nodeb = new AdjnodeCsv(searchRnc.getText(),true);
+                      nodeb.start();
+                      nodeb.setOnSucceeded((e) -> {
+                          if(!CollectionUtils.isEmpty( nodeb.getValue())){
+                              Platform.runLater(() -> {
+                                  nodebList.getItems().clear();
+                                  nodebList.getItems().addAll(nodeb.getValue());
+                              });
+
+                          }
+                      });*/
+               
             });
-            
+             //rncPossibleSuggestions.forEach(System.out::println);
+                cargarNodeB.setDisable(false);
            }
-           //rncPossibleSuggestions.forEach(System.out::println);
-           
+          
     }
    
     @FXML
     void onKeyPressed(KeyEvent event) {
-        if(event.getCode() == KeyCode.BACK_SPACE || event.getCode() == KeyCode.DELETE){
-            if(!searchRnc.getText().trim().isEmpty())
-               rncSearchName = new StringBuilder(searchRnc.getText());
+        /*if(event.getCode() == KeyCode.ENTER ){
+            rncSearchName = new StringBuffer(searchRnc.getText());
+        }*/
+        /*if(event.getCode() == KeyCode.BACK_SPACE || event.getCode() == KeyCode.DELETE){
+            if(rncSearchName.length()>0)
+               this.rncSearchName.setLength(rncSearchName.length()-1);
+        }*/
+    }
+    @FXML
+    void onCargarNodeb(ActionEvent event) {
+        if(!searchRnc.getText().trim().isEmpty()){
             
+            AdjnodeCsv nodeb = new AdjnodeCsv(searchRnc.getText(),true);
+                       nodeb.start();
+                       nodeb.setOnSucceeded((e) -> {
+                           if(!CollectionUtils.isEmpty( nodeb.getValue())){
+                               Platform.runLater(() -> {
+                                   nodebList.getItems().clear();
+                                   nodebList.getItems().addAll(nodeb.getValue());
+                               });
+                               
+                           }
+                           cargarNodeB.setDisable(true);
+                       });
         }
     }
-   
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        cargarNodeB.setDisable(true);
         //Platform.isSupported(ConditionalFeature.INPUT_METHOD);
         // TODO
         /*TextFields.bindAutoCompletion(searchRnc, t -> {
@@ -83,6 +129,7 @@ public class GouScriptViewController implements Initializable {
             }).collect(Collectors.toList());
         });*/
         
+       
     }    
     
 }
